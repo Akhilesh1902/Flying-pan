925d7e047f1cc2f05d98027f36333a119cb688f4
Akhilesh1902 <akhil1922002@gmail.com>
2022-02-16 11:32:34 +0530

    changet BrowserRouter to  HashRouter in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Hero from "./components/hero/Hero";
 import Adventure from "./components/adventure/Adventure";
 import Cards from "./components/cards/Cards";
